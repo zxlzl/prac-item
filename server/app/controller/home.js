a83866046184ee1a6b4306f88e9f2bea01ef8474
zxlzl <384542246@qq.com>
2020-06-20 10:38:12 +0800

    reinit

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require('egg').Controller
 class HomeController extends Controller {
   async index() {
     const { ctx } = this
+    console.log(111)
     ctx.body = 'hi, egg'
   }
 }
