a1b6348cfbcb64f026a74f50b72a93968214fa7f
zxlzl <384542246@qq.com>
2020-07-12 18:13:37 +0800

    chore(home.controller): 测试修改

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -5,7 +5,8 @@ const Controller = require('egg').Controller
 class HomeController extends Controller {
   async index() {
     const { ctx } = this
-    ctx.body = 'hi, egg';
+    console.log(11)
+    ctx.body = 'hi, egg'
   }
 }
 
