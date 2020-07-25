d6dd30e0340b3429e178e18a1de8b0a9594d62cd
zxlzl <384542246@qq.com>
2020-07-25 17:42:15 +0800

    切片

--- front/pages/uc.vue
+++ front/pages/uc.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div>
-      <el-progress :stroke-width="20" :text-inside="true" :percentage="uploadProgress"></el-progress>
+      <!-- <el-progress :stroke-width="20" :text-inside="true" :percentage="uploadProgress"></el-progress> -->
     </div>
     <div>
       <el-button @click="uploadFile">上传</el-button>
@@ -15,12 +15,38 @@
       <p>计算hash的进度</p>
       <el-progress :stroke-width="20" :text-inside="true" :percentage="hashProgress"></el-progress>
     </div>
+
+    <div>
+      <!-- chunk.progress
+      如果progress = -1 或者 <0 报错 显示红色
+      == 100 成功
+      别的数字 方块高度显示-->
+      尽可能让方块看起来是正方体
+      <!-- 比如10个方块 4*4 9个 3*3 -->
+      <div class="cube-container" :style="{width:cubeWidth+'px'}">
+        <div class="cube" v-for="chunk in chunks" :key="chunk.name">
+          <div
+            :class="{
+            'uploading':chunk.progress>0&&chunk.progress<100,
+            'success':chunk.progress==100,
+            'error':chunk.progress<0,
+          }"
+          >
+            <i
+              class="el-icon-loading"
+              style="color:#f48f62"
+              v-if="chunk.progress<100&&chunk.progress>0"
+            ></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import sparkMD5 from "spark-md5";
-import { resolve } from 'url';
+import { resolve } from "url";
 
 const CHUNK_SIZE = 0.1 * 1024 * 1024;
 export default {
@@ -31,10 +57,24 @@ export default {
   data() {
     return {
       file: null,
-      uploadProgress: 0,
+      // uploadProgress: 0,
       hashProgress: 0,
       chunks: [],
     };
+  },
+  computed: {
+    cubeWidth() {
+      return Math.ceil(Math.sqrt(this.chunks.length)) * 16;
+    },
+    uploadProgress() {
+      if (!this.file || this.chunks.length) {
+        return 0;
+      }
+      const loaded = this.chunks
+        .map((item) => item.chunk.size * item.progress)
+        .reduce((acc, cur) => acc + cur,0);
+      return parseInt((loaded / this.file.size)*100).toFixed(2);
+    },
   },
   methods: {
     bindEvents() {
@@ -164,38 +204,38 @@ export default {
         window.requestIdleCallback(workLoop);
       });
     },
-    async calculateHashSimple(){
+    async calculateHashSimple() {
       // 布隆过滤器 判断一个数据存在与否
-      return new Promise(resolve=>{
-        const spark = new sparkMD5.ArrayBuffer()
-        const reader = new FileReader()
-        
-        const file = this.file
-        const size = file.size
-        const offset = 2*1024*1024
+      return new Promise((resolve) => {
+        const spark = new sparkMD5.ArrayBuffer();
+        const reader = new FileReader();
+
+        const file = this.file;
+        const size = file.size;
+        const offset = 2 * 1024 * 1024;
         // 第一个区块也就是第一个2m 最后一个2m 数据全要 中间取前中后各两个字节
-        let chunks = [file.slice(0,offset)]
-        let cur = offset
-        while(cur<size){
-          if (cur+offset>=size) {
-            chunks.push(file.slice(cur,cur+offset))
-          }else{
+        let chunks = [file.slice(0, offset)];
+        let cur = offset;
+        while (cur < size) {
+          if (cur + offset >= size) {
+            chunks.push(file.slice(cur, cur + offset));
+          } else {
             // 中间的区块
-            const mid = cur+offset/2
-            const end = cur+offset
-            chunks.push(file.slice(cur,cur+2))
-            chunks.push(file.slice(mid,mid+2))
-            chunks.push(file.slice(end-2,end))
+            const mid = cur + offset / 2;
+            const end = cur + offset;
+            chunks.push(file.slice(cur, cur + 2));
+            chunks.push(file.slice(mid, mid + 2));
+            chunks.push(file.slice(end - 2, end));
           }
-          cur+=offset
+          cur += offset;
         }
-        reader.readAsArrayBuffer(new Blob(chunks))
-        reader.onload = e=>{
-          spark.append(e.target.result)
-          this.hashProgress = 100
-          resolve(spark.end())
-        }
-      })
+        reader.readAsArrayBuffer(new Blob(chunks));
+        reader.onload = (e) => {
+          spark.append(e.target.result);
+          this.hashProgress = 100;
+          resolve(spark.end());
+        };
+      });
     },
     async uploadFile() {
       // if (!await this.isImage(this.file)) {
@@ -204,16 +244,21 @@ export default {
       // }else{
       //   console.log('格式正确');
       // }
-      this.chunks = this.createFileChunk(this.file);
-      const hash = await this.calculateHashWorker();
-      const hash1 = await this.calculateHashIdle();
-      const hash2 = await this.calculateHashSimple();
-      console.log("文件hash", hash);
-      console.log("文件hash1", hash1);
-      console.log("文件has2 simple hash", hash2);
+      const chunks = this.createFileChunk(this.file);
+      // const hash = await this.calculateHashWorker();
+      // const hash1 = await this.calculateHashIdle();
+      const hash = await this.calculateHashSimple();
+      this.hash = hash;
+      // console.log("文件hash", hash);
+      // console.log("文件hash1", hash1);
+      console.log("文件has2 simple hash", hash);
 
-      // 抽样hash 不算全量
-      // 布隆过滤器 损失一小部分的精度 换取效率
+      this.chunks = chunks.map((chunk, index) => {
+        // 切片名字 hash index
+        const name = hash + "-" + index;
+        return { hash, name, index, chunk: chunk.file };
+      });
+      // await this.uploadChunks();
 
       return;
       const form = new FormData();
@@ -227,20 +272,54 @@ export default {
         },
       });
     },
+    async uploadChunks() {
+      const request = this.chunks
+        .map((item, index) => {
+          const form = new FormData();
+          const { chunk, hash, name } = item;
+          form.append("chunk", chunk);
+          form.append("hash", hash);
+          form.append("name", name);
+          // form.append('index',chunk.index)
+          return form;
+        })
+        .map((form, index) =>
+          this.$http.post("/uploadfile", {
+            onUploadProgress: (progress) => {
+              // 不是整体的进度条 而是每个区块有自己的进度条 整体的需要计算
+              this.chunks[index].progress = Number(
+                (progress.loaded / progress.total) * 100
+              ).toFixed(2);
+            },
+          })
+        );
+      // @todo 并发量控制
+      await Promise.all(request);
+    },
   },
 };
 </script>
 
 <style lang="stylus" scoped>
-#drag {
+#drag 
   height: 100px;
   border: 2px dashed green;
   text-align: center;
   line-height: 100px;
   vertical-align: middle;
+.cube-container 
+  .cube
+    width 14px
+    height 14px
+    line-height 12px
+    border 1px black solid 
+    background #eeeeee
+    float left 
+    >.success
+      background green
+    >.uploading
+      background blue
+    >.error
+      background red
 
-  &:hover {
-    border-color: red;
-  }
-}
 </style>
