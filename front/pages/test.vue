e290d5d44eb742f4502ccdee678d06e209fd3c51
zxlzl <384542246@qq.com>
2020-08-11 20:30:31 +0800

    轮播

---
+++ front/pages/test.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="notice"
+    id="notice"
+  >
+    <ul
+      class="notice-box"
+      :style="{transform:transform}"
+      :class="{unanim:num===0}"
+    >
+      <li
+        class="item"
+        v-for="(item,index) in notices"
+        :key="index"
+      >
+        <span>{{item}}</span>
+      </li>
+      <li
+        class="item"
+        v-for="(item,index) in notices"
+        :key="index+notices.length"
+      >
+        <span>{{item}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      notices: ['内容1', '内容2', '内容3',],
+      num: 0
+    }
+  },
+  computed: {
+    transform: function () {
+      return 'translateY(-' + this.num * 40 + 'px)'
+    }
+  },
+  created: function () {
+    setInterval(()=> {
+      if (this.num !== this.notices.length) {
+        this.num++
+      } else {
+        this.num = 0
+      }
+    }, 2000)
+  }
+};
+</script>
+
+<style scoped>
+.notice{
+    display: inline-block;
+    position:relative;
+    overflow: hidden;
+    height: 40px;
+  }
+  .notice-box{
+    transition: 1s linear;
+  }
+  .item {
+    height: 40px;
+    line-height: 40px;
+  }
+  .unanim{
+    transition: none
+  }
+</style>
